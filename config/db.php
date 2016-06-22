cb021d2cc346f5f4f80848942d067d2a36528b43
wwq <1003792360@qq.com>
2016-06-22 08:51:57 +0800

    v1.0-1

---
+++ config/db.php
@@ -0,0 +1,8 @@
+<?php
+					return [
+						'class' => 'yii\db\Connection',
+						'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=we6',
+						'username' => 'root',
+						'password' => 'root',
+						'charset' => 'utf8',						
+					];
